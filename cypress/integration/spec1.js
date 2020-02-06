c1ec8e7b9c3cdfa8916cc0557f89214e07e920bf
estrada9166 <estrada9166@hotmail.com>
2020-02-06 17:18:26 -0500

    Log job info

--- cypress/integration/spec1.js
+++ cypress/integration/spec1.js
@@ -1,6 +1,7 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
-it('works', () => {
-  cy.wait(5000)
-})
+it("works", () => {
+  cy.wait(5000);
+  throw new Error("Fail");
+});
